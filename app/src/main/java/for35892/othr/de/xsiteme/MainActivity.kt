63be3c8e932c14c3e1b4c4e3163e74e9693f1a89
Robin Forster <robin.forster@st.oth-regensburg.de>
2018-11-05 13:28:42 +0100

    Added the Design library

---
+++ app/src/main/java/for35892/othr/de/xsiteme/MainActivity.kt
@@ -0,0 +1,12 @@
+package for35892.othr.de.xsiteme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
