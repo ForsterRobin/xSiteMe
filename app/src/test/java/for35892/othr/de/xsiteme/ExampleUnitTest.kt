5281a062d852a140ce95f0d7444fab16dbbac4b1
Robin Forster <robin.forster@st.oth-regensburg.de>
2018-11-05 13:28:42 +0100

    Initial commit

---
+++ app/src/test/java/for35892/othr/de/xsiteme/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package for35892.othr.de.xsiteme
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
